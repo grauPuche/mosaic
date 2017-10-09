d90948f3efdd2c7de1459e552165eb4c113882df
grauPuche <talk@graupuche.info>
2017-10-09 14:09:41 -0400

    1st commt

---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var video = document.querySelector('video');
+
+// Put variables in global scope to make them available to the browser console.
+var constraints = window.constraints = {
+  audio: false,
+  video: true
+};
+
+function handleSuccess(stream) {
+  var videoTracks = stream.getVideoTracks();
+  console.log('Got stream with constraints:', constraints);
+  console.log('Using video device: ' + videoTracks[0].label);
+  stream.oninactive = function() {
+    console.log('Stream inactive');
+  };
+  window.stream = stream; // make variable available to browser console
+  video.srcObject = stream;
+}
+
+navigator.mediaDevices.getUserMedia(constraints);
